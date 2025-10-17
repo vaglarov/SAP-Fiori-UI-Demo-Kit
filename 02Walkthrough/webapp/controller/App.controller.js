ceedf31aae854fb1ee3399093caa42ac6b32dd73
Boyan Vaglarov <49076050+vaglarov@users.noreply.github.com>
2025-10-17 09:40:52 +0300

    Add files via upload

---
+++ 02Walkthrough/webapp/controller/App.controller.js
@@ -0,0 +1,24 @@
+sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast",
+   "sap/ui/model/json/JSONModel"
+], (Controller, MessageToast, JSONModel) => {
+   "use strict";
+
+   return Controller.extend("ui5.walkthrough.controller.App", {
+      onInit() {
+         // set data model on view
+         const oData = {
+            recipient : {
+               name : "World"
+            }
+         };
+         const oModel = new JSONModel(oData);
+         this.getView().setModel(oModel);
+      },
+
+      onShowHello() {
+         MessageToast.show("Hello World");
+      }
+   });
+});
