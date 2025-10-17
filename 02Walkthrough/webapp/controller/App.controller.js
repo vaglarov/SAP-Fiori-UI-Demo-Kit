d60acf5e2fcbd9942a33bf654a1bd9c1bc9bf38f
Boyan Vaglarov <49076050+vaglarov@users.noreply.github.com>
2025-10-17 16:01:04 +0300

    Add files via upload

--- 02Walkthrough/webapp/controller/App.controller.js
+++ 02Walkthrough/webapp/controller/App.controller.js
@@ -1,24 +1,8 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller",
-   "sap/m/MessageToast",
-   "sap/ui/model/json/JSONModel"
-], (Controller, MessageToast, JSONModel) => {
+   "sap/ui/core/mvc/Controller"
+], (Controller) => {
    "use strict";
 
    return Controller.extend("ui5.walkthrough.controller.App", {
-      onInit() {
-         // set data model on view
-         const oData = {
-            recipient : {
-               name : "World"
-            }
-         };
-         const oModel = new JSONModel(oData);
-         this.getView().setModel(oModel);
-      },
-
-      onShowHello() {
-         MessageToast.show("Hello World");
-      }
    });
 });
--- 02Walkthrough/webapp/index.js
+++ 02Walkthrough/webapp/index.js
@@ -1,9 +1,13 @@
 sap.ui.define([
-	"sap/ui/core/mvc/XMLView"
-], (XMLView) => {
+	"sap/ui/core/ComponentContainer"
+], (ComponentContainer) => {
 	"use strict";
 
-	XMLView.create({
-		viewName: "ui5.walkthrough.view.App"
-	}).then((oView) => oView.placeAt("content"));
+	new ComponentContainer({
+		name: "ui5.walkthrough",
+		settings : {
+			id : "walkthrough"
+		},
+		async: true
+	}).placeAt("content");
 });
