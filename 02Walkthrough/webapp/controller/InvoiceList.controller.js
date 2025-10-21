3d12c82b35b76f403a5c10382d027ac07bc16743
Boyan Vaglarov <49076050+vaglarov@users.noreply.github.com>
2025-10-21 15:06:19 +0300

    Add files via upload

--- 02Walkthrough/webapp/controller/InvoiceList.controller.js
+++ 02Walkthrough/webapp/controller/InvoiceList.controller.js
@@ -26,6 +26,14 @@ sap.ui.define([
 			const oList = this.byId("invoiceList");
 			const oBinding = oList.getBinding("items");
 			oBinding.filter(aFilter);
+		},
+
+		onPress(oEvent) {
+			const oItem = oEvent.getSource();
+			const oRouter = this.getOwnerComponent().getRouter();
+			oRouter.navTo("detail", {
+				invoicePath: window.encodeURIComponent(oItem.getBindingContext("invoice").getPath().substring(1))
+			});
 		}
 	});
 });
