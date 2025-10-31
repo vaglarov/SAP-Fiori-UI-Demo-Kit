97b48a82480f1f62fc2ba20058dac9dff0d8ff03
Boyan Vaglarov <49076050+vaglarov@users.noreply.github.com>
2025-10-31 09:25:12 +0200

    Add files via upload

---
+++ 101ReadySteadyGo/webapp/index.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"sap/ui/core/mvc/XMLView"
+], (XMLView) => {
+	"use strict";
+
+	XMLView.create({
+		viewName: "ui5.quickstart.App"
+	}).then((oView) => oView.placeAt("content"));
+
+});
