93460e7abefbad1480c9068ee5eb92b355e169e6
Boyan Vaglarov <49076050+vaglarov@users.noreply.github.com>
2025-11-04 10:13:32 +0200

    Add files via upload

---
+++ 102Walkthrough/webapp/controller/InvoiceList.controller.js
@@ -0,0 +1,45 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], (Controller, JSONModel, Filter, FilterOperator) => {
+	"use strict";
+
+	return Controller.extend("ui5.walkthrough.controller.InvoiceList", {
+		onInit() {
+			const oViewModel = new JSONModel({
+				currency: "EUR"
+			});
+			this.getView().setModel(oViewModel, "view");
+		},
+
+		 async onOpenDialog() {
+            // create dialog lazily
+            this.oDialog ??= await this.loadFragment({
+                name: "ui5.walkthrough.view.HelloDialog"
+            });
+        
+            this.oDialog.open();
+        },
+        onCloseDialog() {
+			// note: We don't need to chain to the pDialog promise, since this event handler
+			// is only called from within the loaded dialog itself.
+			this.byId("helloDialog").close();
+		},
+
+		onFilterInvoices(oEvent) {
+			// build filter array
+			const aFilter = [];
+			const sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+			}
+
+			// filter binding
+			const oList = this.byId("invoiceList");
+			const oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
+		}
+	});
+});
